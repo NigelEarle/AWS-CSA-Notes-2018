4b081594f01047511528771840e55be6be0e7cd8
nwre87 <nwre87@gmail.com>
2018-07-05 20:14:50 -0700

    Build a serverless website assets

---
+++ EC2/templates/Build-A-Serverless-Website/hellocloudgurus.py
@@ -0,0 +1,12 @@
+def lambda_handler(event, context):
+    print("In lambda handler")
+    
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Ryan Kroonenburg"
+    }
+    
+    return resp
