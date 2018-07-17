298abd85667b271088c3724b28c5cb73b42119ac
nwre87 <nwre87@gmail.com>
2018-07-16 22:33:36 -0700

    Add WP bootstrap script to templates

---
+++ EC2/templates/bootstrapscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum install httpd php php-mysql -y
+cd /var/www/html
+echo "healthy" > healthy.html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+rm -rf wordpress
+rm -rf latest.tar.gz
+chmod -R 755 wp-content
+chown -R apache:apache wp-content
+wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt
+mv htaccess.txt .htaccess
+chkconfig httpd on
